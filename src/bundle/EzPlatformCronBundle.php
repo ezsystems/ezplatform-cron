40fed0d16270d002f1edf578f82e56498c5914da
Maciej Kobus <maciej.kobus@ez.no>
2018-05-24 15:08:17 +0200

    EZP-29240: Change package namespace and directory structure in ezplatform-cron package

--- src/bundle/EzPlatformCronBundle.php
+++ src/bundle/EzPlatformCronBundle.php
@@ -3,13 +3,13 @@
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
-namespace EzSystems\CronBundle;
+namespace EzSystems\EzPlatformCronBundle;
 
-use EzSystems\CronBundle\DependencyInjection\Compiler\CronJobCompilerPass;
+use EzSystems\EzPlatformCronBundle\DependencyInjection\Compiler\CronJobCompilerPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class EzSystemsCronBundle extends Bundle
+class EzPlatformCronBundle extends Bundle
 {
     public function build(ContainerBuilder $container)
     {
